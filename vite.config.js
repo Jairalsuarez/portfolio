5dfb548c52b3bcf6373a167120386bc26e02e319
Jaira Suarez <jairsuarez9806@gmail.com>
2026-03-19 19:48:04 -0500

    v2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        // Tu página principal
+        main: resolve(__dirname, 'index.html'),
+        // Tu nueva página de proyectos
+        proyectos: resolve(__dirname, 'pages/bienvenida.html')
+      }
+    }
+  }
 })
